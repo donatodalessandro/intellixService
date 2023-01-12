e7e1bbc988aeeff5e286cb61b3a42f75c7373b36
Donato D'Alessandro <donato.dalessandro0798@gmail.com>
2023-01-12 19:13:36 +0100

    Refactor models.py

--- models.py
+++ models.py
@@ -2,12 +2,22 @@ from typing import Optional
 from py_dto import DTO
 
 class ScheduleCommand(DTO):
+
+    """
+       Comando per veicolare la query su cui istanziare lo scheduler
+    """
     query: str
 
 class SearchCommand(DTO):
+    """
+       Comando per veicolare la query e eventuali date di inizio e di fine
+    """
     query: str
     fromDate: Optional[int]
     toDate: Optional[int]
 
 class TokenCommand(DTO):
+    """
+       Comando per veicolare il token per abilitare la ricerca su intelx
+    """
     token: str
